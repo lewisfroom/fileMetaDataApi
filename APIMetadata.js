994d8b9cad5ba449f74b9c417a97ccc29b0ecf93
lewisfroom <mrmindazo@gmail.com>
2017-01-06 12:52:43 +0000

    Initial commit of working a metadata API, providing the file size of an uploaded file.

---
+++ APIMetadata.js
@@ -0,0 +1,12 @@
+var express = require("express");
+var app = express();
+var multer  = require('multer');
+var upload = multer({ dest: 'uploads/' });
+
+app.post("/get-file-size", upload.single("file"), function(req, res){
+    res.set("Content-Type", "application/json");
+    res.send(JSON.stringify({"size" : req.file.size}));
+});
+
+app.use(express.static('public'));
+app.listen(process.env.PORT, process.env.IP);
