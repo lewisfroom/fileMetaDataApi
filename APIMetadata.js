3da2ae4cee069177923d05fc631b4a5028cf91bf
lewisfroom <mrmindazo@gmail.com>
2017-01-06 13:03:10 +0000

    Added function to delete file after it has been processed

--- APIMetadata.js
+++ APIMetadata.js
@@ -1,11 +1,13 @@
 var express = require("express");
 var app = express();
+var fs = require("fs");
 var multer  = require('multer');
 var upload = multer({ dest: 'uploads/' });
 
 app.post("/get-file-size", upload.single("file"), function(req, res){
     res.set("Content-Type", "application/json");
     res.send(JSON.stringify({"size" : req.file.size}));
+    fs.unlink(req.file.path)
 });
 
 app.use(express.static('public'));
